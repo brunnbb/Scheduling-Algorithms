da33c91b1b37b960777454a3d337e8ad1fda6232
Bruno <brunobbianchini@gmail.com>
2024-07-11 23:05:19 -0300

    Added main class

--- demo/src/main/java/com/example/main/Main.java
+++ demo/src/main/java/com/example/main/Main.java
@@ -1,7 +1,15 @@
 package com.example.main;
 
+import com.example.computer.*;
+import com.example.resources.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Choose your json
+        JsonParser jsonParser = new JsonParser("demo\\src\\main\\java\\com\\example\\jsons\\slideFcfs.json");
+        Simulation simulation = jsonParser.readJson();
+        Scheduler scheduler = new Scheduler(simulation);
+        scheduler.start();
     }
+
 }
